8c97993e23cfe2f2a2f0ee6b227fdef8e55790cc
Solal <arroues10@gmail.com>
2021-11-14 22:35:13 +0200

    name

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/<angular-autoCFE>'));
+app.use(express.static(__dirname + '/dist/<angular-auto-cfe>'));
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/<angular-autoCFE>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/<angular-auto-cfe>/index.html'));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
